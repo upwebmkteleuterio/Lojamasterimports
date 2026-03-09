ac89c901f57cbe04f8414cc59a17ba1820d61e25
[dyad] <maiconeleuterio@gmail.com>
2026-03-09 12:07:13 -0300

    [dyad] Instalando embla-carousel-autoplay para corrigir erro de importação. - wrote 3 file(s), added embla-carousel-autoplay package(s)

--- src/hooks/useCategoryCarousel.ts
+++ src/hooks/useCategoryCarousel.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from 'react';
+import { useCallback } from 'react';
 import useEmblaCarousel from 'embla-carousel-react';
 import Autoplay from 'embla-carousel-autoplay';
 
